06587c60a65301edff593e827fbb4e4d50f702b1
navod99 <navx99@gmail.com>
2022-10-11 03:07:03 +0530

    Search function

--- frontend/src/Components/eventScheduling/EventScheduling.js
+++ frontend/src/Components/eventScheduling/EventScheduling.js
@@ -9,39 +9,94 @@ import AddEvents from './AddEvents';
 import Grid from '@mui/material/Grid';
 import axios from 'axios'
 import EventList from './EventList';
+import { useSelector } from 'react-redux';
 
 const EventScheduling = () => {
 
     const [events, setEvents] = useState([])
     const [toggle, setToggle] = useState(false)
+    const [searchTerm, setSearchTerm] = useState("");
+    const status = useSelector(state => state.filterEvents.status)
+    const year = useSelector(state => state.filterEvents.year)
 
     useEffect(() => {
-        function getEvents() {
-            axios.get(`http://localhost:5000/eventScheduling/viewevents`).then((res) => {
-                console.log(res.data)
-                setEvents(res.data)
+        if (!searchTerm && !status && !year) {
+            function getEvents() {
+                axios.get(`http://localhost:5000/eventScheduling/viewevents`).then((res) => {
+                    console.log(res.data)
+                    setEvents(res.data)
 
-            }).catch((err) => {
-                alert(err.message);
-                console.log(err.message);
-            })
+                }).catch((err) => {
+                    alert(err.message);
+                    console.log(err.message);
+                })
+            }
+            getEvents()
         }
-        getEvents()
 
-    }, [toggle])
+    }, [toggle, searchTerm, status,year])
+
+    const findEvents = (eventName) => {
+        if (eventName) {
+            axios.get(`http://localhost:5000/eventScheduling/search/${eventName}`)
+
+                .then((res) => {
+                    let arr = res.data;
+                    let i;
+                    let list = [];
+                    for (i = 0; i < arr.length; i++) {
+                        list.push(arr[i]);
+                    }
+                    setEvents(list)
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+
+        }
+    };
+
+    useEffect(() => {
+        let query
+        if (status && year) {
+            query = `?eventStatus=${status}&year=${year}`
+        }
+        else if (status) {
+            query = `?eventStatus=${status}`
+        }
+        else if (year) {
+            query = `?year=${year}`
+        }
+        axios.get(`http://localhost:5000/eventScheduling/filter${query}`)
+        .then((res) => {
+            console.log(res.data, "res.data")
+            let arr = res.data;
+            let i;
+            let list = [];
+            for (i = 0; i < arr.length; i++) {
+                list.push(arr[i]);
+            }
+            setEvents(list)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    }, [status, year])
+
 
     return (
         <>
             <Container sx={{ ml: 40 }}>
-                <SearchBar />
+                <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} findEvents={findEvents} />
             </Container>
             <Container sx={{ mt: 4 }}>
                 <Grid container spacing={3}>
                     <Grid item xs={12} md={8} lg={2}>
-                        <FilterStatus />
+                        <FilterStatus/>
                     </Grid>
                     <Grid item xs={12} md={8} lg={2}>
-                        <FilterYear />
+                        <FilterYear/>
                     </Grid>
                     <Grid item xs={12} md={8} lg={2}>
                         <GenerateReports />
@@ -51,7 +106,7 @@ const EventScheduling = () => {
                     </Grid>
                 </Grid>
             </Container>
-            <EventList events={events} />
+            <EventList events={events} setToggle={setToggle} toggle={toggle} />
 
         </>
     )
