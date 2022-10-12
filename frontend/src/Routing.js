ff2b88ced5fc6c4d352fc84146c332ba17fc011c
navod99 <navx99@gmail.com>
2022-10-12 19:26:00 +0530

    Implement home page

--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -8,11 +8,13 @@ import Dashboard from './Components/AdminDashboard';
 import EventScheduling from './Components/eventScheduling/EventScheduling';
 import Projects from './Components/projects/projects';
 import ProjectDes from './Components/projects/ProjectDes';
+import Home from './Components/Home';
 
 const Routing = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' exact element={<Home/>} />
                 <Route path='/adminDashboard' exact element={<Dashboard />} />
                 <Route path='/events' exact element={<EventScheduling />} />
                 <Route path='/projects' exact element={<Projects />} />
